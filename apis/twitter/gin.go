9d26dade28914ac2f51f7091bac7f76317bb8ef3
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-10-28 08:43:42 +0200

    feat: added the twitter/authors endpoint

--- apis/twitter/gin.go
+++ apis/twitter/gin.go
@@ -10,6 +10,15 @@ import (
 func RegisterGinHandler(r *gin.Engine, cfg *Config) {
 	twitterHandler := NewHandler(cfg.CacheFilePath, NewAPI(cfg.Bearer))
 	r.Group("/twitter").
+		GET("/authors/:tweet_id", func(c *gin.Context) {
+			tweet, err := twitterHandler.GetTweet(c.Param("tweet_id"))
+			if err != nil {
+				c.AbortWithError(http.StatusBadRequest, err)
+				return
+			}
+
+			c.JSON(http.StatusOK, &tweet.Author)
+		}).
 		GET("/tweets/:id", func(c *gin.Context) {
 			tweet, err := twitterHandler.GetTweet(c.Param("id"))
 			if err != nil {
