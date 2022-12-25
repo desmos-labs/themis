836d0108459a7b2a2a1e2ef22128b44f5faa2d42
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-12-25 12:39:58 -0600

    feat: improve the YouTube APIs to also search for the username

--- apis/youtube/api.go
+++ apis/youtube/api.go
@@ -3,12 +3,19 @@ package youtube
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strings"
 )
 
-var fields = []string{"items.id", "items.snippet.title", "items.snippet.description", "items.snippet.publishedAt"}
+var (
+	fields = strings.Join([]string{
+		"items.id",
+		"items.snippet.title",
+		"items.snippet.description",
+		"items.snippet.publishedAt",
+	}, ",")
+)
 
 // API allows to query data from the Twitter APIs
 type API struct {
@@ -46,45 +53,64 @@ func (api *API) runRequest(req *http.Request) ([]byte, error) {
 	}
 
 	// Parse the body
-	return ioutil.ReadAll(resp.Body)
+	return io.ReadAll(resp.Body)
 }
 
-// GetChannel returns the details of the Channel having the given id
-func (api *API) GetChannel(id string) (Channel, error) {
-	// Build the request endpoint. Example:
-	// https://www.googleapis.com/youtube/v3/channels?id=<channel-id>&part=snippet&fields=items.id,items.snippet.title,items.snippet.description,items.snippet.publishedAt
-	endpoint := fmt.Sprintf("%s/channels?id=%s&part=snippet&fields=%s", api.endpoint, id, strings.Join(fields, ","))
+func (api *API) getChannel(searchQuery string) (*Channel, error) {
+	endpoint := fmt.Sprintf("%s/channels?%s&part=snippet&fields=%s", api.endpoint, searchQuery, fields)
 
 	// Create the request
 	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
-		return Channel{}, err
+		return nil, err
 	}
 
 	// Run the request
 	bz, err := api.runRequest(req)
 	if err != nil {
-		return Channel{}, err
+		return nil, err
 	}
 
+	// Unmarshal the response
 	var response channelResponseJSON
 	err = json.Unmarshal(bz, &response)
 	if err != nil {
-		return Channel{}, err
+		return nil, err
 	}
+
 	return convertChannelResponse(response)
 }
 
-// convertChannelResponse converts the given channelResponseJSON into a Channel object
-func convertChannelResponse(response channelResponseJSON) (Channel, error) {
-	if len(response.Items) != 1 {
-		return Channel{}, fmt.Errorf("invalid number of channels returned: %d", len(response.Items))
+// GetChannel returns the details of the Channel having the given id or username
+func (api *API) GetChannel(search string) (*Channel, error) {
+	// Try searching with the ID
+	channel, err := api.getChannel(fmt.Sprintf("id=UC%s", search))
+	if err != nil {
+		return nil, err
 	}
-	channel := response.Items[0]
+
+	// If not found, try searching with the username
+	if channel == nil {
+		channel, err = api.getChannel(fmt.Sprintf("forUsername=%s", search))
+	}
+
+	return channel, err
+}
+
+// convertChannelResponse converts the given channelResponseJSON into a Channel object
+func convertChannelResponse(response channelResponseJSON) (*Channel, error) {
+	if len(response.Items) == 0 {
+		return nil, nil
+	}
+
+	if len(response.Items) > 1 {
+		return nil, fmt.Errorf("invalid number of channels returned: %d", len(response.Items))
+	}
+
 	return NewChannel(
-		channel.ID,
-		channel.Snippet.Title,
-		channel.Snippet.Description,
-		channel.Snippet.PublishedAt,
+		response.Items[0].ID,
+		response.Items[0].Snippet.Title,
+		response.Items[0].Snippet.Description,
+		response.Items[0].Snippet.PublishedAt,
 	), nil
 }
